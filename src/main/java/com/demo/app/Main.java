27e1f75ee0bcf7de00fbcc0d5c486f49d9add11d
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2024-11-12 11:58:57 -0500

    EXAAMPLE

---
+++ src/main/java/com/demo/app/Main.java
@@ -0,0 +1,15 @@
+package com.demo.app;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println("Hello World! The current time is: " + now);
+
+
+    }
+
+}
